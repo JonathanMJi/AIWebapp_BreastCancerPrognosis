c55799cf4c90cdd3e2477e37d7835764372f4e19
Jonathan Ji <99520267+JonathanMJi@users.noreply.github.com>
2022-08-30 21:16:43 -0500

    Update cnn_app.py

--- cnn_app.py
+++ cnn_app.py
@@ -112,7 +112,8 @@ def main():
 
         prediction_selected = option_menu(
             menu_title="",
-            options=["Predict with Key Values", "Predict with Uploaded File"],
+            #options=["Predict with Key Values", "Predict with Uploaded File"],
+            options=["Predict with Uploaded File"],
             default_index=0,
             orientation="horizontal",
             styles={
